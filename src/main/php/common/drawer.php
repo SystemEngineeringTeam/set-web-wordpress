58302739c451c95a354f7921a3234dbb5dfb364d
k14041kk <k14041kk@aitech.ac.jp>
2016-10-21 18:14:21 +0900

    追加：処理を分離

---
+++ src/main/php/common/drawer.php
@@ -0,0 +1,6 @@
+<div class="mdl-layout__drawer">
+<span class="mdl-layout-title">システム工学研究会</span>
+<nav class="mdl-navigation">
+  <?php get_template_part('common/commonNavigation'); ?>
+</nav>
+</div>
---
+++ src/main/php/common/header.php
@@ -0,0 +1,13 @@
+<header class="mdl-layout__header mdl-layout__header--waterfall">
+  <div class="mdl-layout__header-row">
+    <!-- Title -->
+    <span class="mdl-layout-title"> <a href="../index.html">システム工学研究会</a>
+    </span>
+    <!-- Add spacer, to align navigation to the right -->
+    <div class="mdl-layout-spacer"></div>
+    <!-- Navigation. We hide it in small screens. -->
+    <nav class="mdl-navigation mdl-layout--large-screen-only">
+      <?php get_template_part('common/commonNavigation'); ?>
+    </nav>
+  </div>
+</header>
