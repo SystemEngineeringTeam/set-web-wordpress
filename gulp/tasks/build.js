93b3d8b9915c0902e03f6fe164dbf5d6070c77de
k14041kk <k14041kk@aitech.ac.jp>
2016-10-21 18:15:29 +0900

    追加：画像の処理の登録

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
-var buildTasks = ['buildPHP','buildCSS','buildScreenshot'];
+var buildTasks = ['buildPHP','buildCSS','buildScreenshot','buildImage'];
 
 gulp.task('build',buildTasks ,function() {
 	
