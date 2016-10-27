d1d802ed0413fb8d2d55b516ce9b8462a3b7a576
k14041kk <k14041kk@aitech.ac.jp>
2016-10-27 13:09:06 +0900

    修正：cssのビルド設定

--- gulp/tasks/buildCSS.js
+++ gulp/tasks/buildCSS.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 
 gulp.task('buildCSS', function() {
 
-    var css_src = ["./src/main/css/**/*.css"];
+    var css_src = ["./src/main/css/**/*.css", "!./src/main/css/style.css"];
+	gulp.src(css_src)
+	.pipe(gulp.dest("./dist/css/"));
+	
+	var css_src = ["./src/main/css/style.css"];
 	gulp.src(css_src)
 	.pipe(gulp.dest("./dist/"));
 
