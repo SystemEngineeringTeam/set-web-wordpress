ab5dff5a2c6f90d38036ef17b9dbc780bc6a7328
k14041kk <k14041kk@aitech.ac.jp>
2016-11-02 18:41:24 +0900

    追加：rssに画像を

--- src/main/php/functions.php
+++ src/main/php/functions.php
@@ -17,6 +17,15 @@ if ( function_exists('register_sidebar') ){
  ));
 }
 
-
+//アイキャッチをrssに
+function rss_post_thumbnail($content) {
+ global $post;
+ if(has_post_thumbnail($post->ID)) {
+  $content = '<p class="thumbnail">' . get_the_post_thumbnail($post->ID) . '</p>' . $content;
+ }
+ return $content;
+}
+add_filter('the_excerpt_rss', 'rss_post_thumbnail');
+add_filter('the_content_feed', 'rss_post_thumbnail');
 
 ?>
